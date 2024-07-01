bb98d57ad4dfd3f253eee800e790c4ac4557d524
whitney-shisia <shisiawhitney215@gmail.com>
2024-07-01 08:16:29 +0300

    flask

--- server/models.py
+++ server/models.py
@@ -6,4 +6,9 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
